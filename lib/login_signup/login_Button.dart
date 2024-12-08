56ebbc940df5d306ceb765d561c07992cd2aa74a
Chantelle Cabanilla <chanillie11@gmail.com>
2024-12-08 13:01:50 -0800

    attempting...

--- lib/login_signup/login_Button.dart
+++ lib/login_signup/login_Button.dart
@@ -3,26 +3,28 @@ import 'package:flutter/material.dart';
 class LoginButton extends StatelessWidget {
   final String text;
   final double paddingLT;
-  const LoginButton({
-    super.key,
-    required this.text,
-    required this.paddingLT
-  });
+  final VoidCallback onTap;
+
+  const LoginButton(
+      {super.key,
+      required this.text,
+      required this.paddingLT,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: EdgeInsets.fromLTRB(paddingLT, 10, paddingLT, 10),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(
-        text, 
-        style: const TextStyle(
-          color: Colors.white, 
-          fontSize: 20,
-          fontWeight: FontWeight.bold))
-    );
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            padding: EdgeInsets.fromLTRB(paddingLT, 10, paddingLT, 10),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(text,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold))));
   }
 }
